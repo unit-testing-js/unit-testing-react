5520bc003a8569fe4a514109753c9a5877b9a2a9
grh-gitee <3494242908@qq.com>
2023-03-15 21:52:06 +0800

    feat: 优化测试控件

--- src/layout/mainContainer.tsx
+++ src/layout/mainContainer.tsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react"
 import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
 import type { CMM, MenuObject } from './type'
+import { getIcon } from './icon'
 import './index.less'
+import { isUndefined } from "check-it-type"
 export interface MainContainer extends CMM {
 	menu: MenuObject[]
+	/**
+	 * @default true
+	 */
+	fold?: boolean
 }
 
 export function MainContainer(props: MainContainer) {
 	const { menu = [] } = props
 	const [select, setSelect] = useState<string>('')
+	const [fold, setFold] = useState<boolean>(isUndefined(props.fold) ? true : props.fold)
+
 
 	useEffect(() => {
 		const names = /(\w+)$/.exec(location.href)
@@ -19,23 +27,45 @@ export function MainContainer(props: MainContainer) {
 
 	return (
 		<BrowserRouter basename="/">
-			<div className="main">
+			<div
+				className="main"
+				style={{
+					gridTemplateColumns: fold ? '60px 1fr' : '10vw 1fr',
+					transition: 'all .2s ease-in-out',
+				}}>
 				<aside className="menu">
 					{menu.map((item: MenuObject) => {
 						const { name, path } = item
-						if (path && path !== '/')
+						if (path && path !== '/') {
+							const _name = name || path.replace('/', '')
+							const showName = fold ? (_name[0]) : (_name)
 							return <Link
 								to={path}
 								className={select === name ? 'isSelect' : ''}
+								style={fold ? {
+									textAlign: 'center',
+									fontSize: '18px',
+									fontWeight: 'bold',
+								} : {}}
+								title={name}
 								key={name + path}
 								onClick={() => {
 									name && setSelect(name)
 								}}>
-								{name || path.replace('/', '')}
+								{showName}
 							</Link>
+						}
 					})}
 				</aside>
 				<div className="docs-component-content">
+					<div className="header">
+						<button
+							onClick={() => {
+								setFold(!fold)
+							}}>
+							{getIcon(fold ? 'fold' : 'unFold', 32, '#c5c5c5')}
+						</button>
+					</div>
 					<Routes>
 						{menu.map(router => (<Route
 							key={router.path}
